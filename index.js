f70d473dff952380146868875fca48271a3b25a1
Szilard Huber <szilard.huber@gmail.com>
2013-12-04 17:19:14 +0100

    Added textarea for editing plan.

--- index.js
+++ index.js
@@ -1,73 +1,119 @@
 (function() {
-  var Wall;
+  var Plan, Wall;
+
+  Plan = (function() {
+    var ASPECT, FAR, HEIGHT, NEAR, VIEW_ANGLE, WIDTH;
+
+    WIDTH = 400;
+
+    HEIGHT = 300;
+
+    VIEW_ANGLE = 45;
+
+    ASPECT = WIDTH / HEIGHT;
+
+    NEAR = 0.1;
+
+    FAR = 10000;
+
+    function Plan() {
+      this.camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+      this.camera.rotation.x = -0.7;
+      this.camera.position.z = 600;
+      this.camera.position.y = 600;
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(WIDTH, HEIGHT);
+      this.scene = new THREE.Scene();
+      this.scene.add(this.camera);
+      this.pointLight = new THREE.AmbientLight(0xEEEEEE);
+      this.pointLight.position.x = 10;
+      this.pointLight.position.y = 50;
+      this.pointLight.position.z = 130;
+      this.scene.add(this.pointLight);
+      this.stage = new Kinetic.Stage({
+        container: floorplan,
+        width: WIDTH,
+        height: HEIGHT,
+        scale: {
+          x: 1,
+          y: -1
+        },
+        offset: {
+          x: -50,
+          y: 250
+        }
+      });
+      this.layer = new Kinetic.Layer;
+      this.stage.add(this.layer);
+    }
+
+    Plan.prototype.reset = function() {
+      return this.layer.removeChildren();
+    };
+
+    Plan.prototype.draw = function() {
+      this.renderer.render(this.scene, this.camera);
+      return this.layer.batchDraw();
+    };
+
+    Plan.prototype.add = function(object) {
+      this.scene.add(object.mesh);
+      this.layer.add(object.polygon);
+      return this.draw();
+    };
+
+    return Plan;
+
+  })();
 
   $(function() {
-    var $container, ASPECT, FAR, HEIGHT, NEAR, VIEW_ANGLE, WIDTH, camera, layer, object, objects, pointLight, renderer, scene, stage, _i, _j, _len, _len1;
+    var $container, ASPECT, FAR, HEIGHT, NEAR, VIEW_ANGLE, WIDTH, plan;
     WIDTH = 400;
     HEIGHT = 300;
     VIEW_ANGLE = 45;
     ASPECT = WIDTH / HEIGHT;
     NEAR = 0.1;
     FAR = 10000;
-    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-    camera.rotation.x = -0.7;
+    plan = new Plan();
     $('body').keypress(function(event) {
       switch (event.charCode) {
         case 119:
-          camera.position.z -= 10;
-          break;
-        case 115:
-          camera.position.z += 10;
+          plan.camera.position.z -= 10;
           break;
         case 97:
-          camera.position.x += 10;
+          plan.camera.position.x += 10;
+          break;
+        case 115:
+          plan.camera.position.z += 10;
           break;
         case 100:
-          camera.position.x -= 10;
+          plan.camera.position.x -= 10;
       }
-      return renderer.render(scene, camera);
+      return plan.renderer.render(plan.scene, plan.camera);
+    });
+    $('#text').change(function(event) {
+      var content, line, lines, object, tokens, _i, _len, _results;
+      plan.reset();
+      content = event.target.value;
+      lines = content.split('\n');
+      _results = [];
+      for (_i = 0, _len = lines.length; _i < _len; _i++) {
+        line = lines[_i];
+        tokens = line.split(',');
+        if (tokens[0].trim().toLowerCase() === 'wall') {
+          object = new Wall(parseInt(tokens[1].trim()), parseInt(tokens[2].trim()), parseInt(tokens[3].trim()), parseInt(tokens[4].trim()), parseInt(tokens[5].trim()), parseInt(tokens[6].trim()));
+          console.log(object);
+          _results.push(plan.add(object));
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
     });
     $container = $("#container");
-    renderer = new THREE.WebGLRenderer();
-    scene = new THREE.Scene();
-    scene.add(camera);
-    camera.position.z = 600;
-    camera.position.y = 600;
-    renderer.setSize(WIDTH, HEIGHT);
-    $container.append(renderer.domElement);
-    objects = [];
-    objects[0] = new Wall(0, 240, 270, 240, 270, 44);
-    objects[1] = new Wall(0, 0, 0, 240, 270, 44);
-    objects[2] = new Wall(270, 240, 270, 0, 270, 10);
-    for (_i = 0, _len = objects.length; _i < _len; _i++) {
-      object = objects[_i];
-      scene.add(object.mesh);
-    }
-    pointLight = new THREE.AmbientLight(0xEEEEEE);
-    pointLight.position.x = 10;
-    pointLight.position.y = 50;
-    pointLight.position.z = 130;
-    scene.add(pointLight);
-    renderer.render(scene, camera);
-    stage = new Kinetic.Stage({
-      container: floorplan,
-      width: WIDTH,
-      height: HEIGHT,
-      scale: {
-        x: 1,
-        y: -1
-      },
-      offset: {
-        x: -50,
-        y: 250
-      }
-    });
-    layer = new Kinetic.Layer;
-    for (_j = 0, _len1 = objects.length; _j < _len1; _j++) {
-      object = objects[_j];
-      layer.add(object.polygon);
-    }
-    return stage.add(layer);
+    $container.append(plan.renderer.domElement);
+    $('textarea#text').change();
+    return plan.draw();
   });
 
   Wall = (function() {
